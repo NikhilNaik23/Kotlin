c9b5dfce90b9db451d3e418aee347515e58aa118
Nikhil <nenavathnikhil2@gmail.com>
2024-08-21 10:28:03 +0530

    Variable_Initialization

--- src/Kotlin.kt
+++ src/Kotlin.kt
@@ -1,12 +1,8 @@
 fun main(args:Array<String>) {
-    var a=40
-    println(a)
-    var `try`=20
-    println(`try`)
-        val b=20
-        print(b)
-//    b=b+10
-        var ab=40
-        var ba:Int
-        println(ab)
-    }
+    val b=20
+    print(b)
+    // b=b+10
+    var ab=40
+    var ba:Int
+    println(ab)
+}

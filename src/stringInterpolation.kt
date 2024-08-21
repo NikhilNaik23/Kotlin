c1404290cc37468473ec50b6c10e51ee99d197d7
Nikhil <nenavathnikhil2@gmail.com>
2024-08-21 13:18:17 +0530

    Updation

--- src/stringInterpolation.kt
+++ src/stringInterpolation.kt
@@ -36,4 +36,8 @@ Nikhil  Nikhil
 
     var numbers= arrayOf(1,2,3,4,5)
     val words:Array<String> =arrayOf("Kotlin","CPP","JAVA")
+    var firstNumber = numbers[0]
+    var secondNumber = numbers[1]
+    println("First Number: $firstNumber")
+    println("Second Number: $secondNumber")
 }

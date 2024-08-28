7f56d5401205517e475b2bc625bd0f6f98c72264
Nikhil <nenavathnikhil2@gmail.com>
2024-08-28 15:39:52 +0530

    Temperatures

--- src/Functions.kt
+++ src/Functions.kt
@@ -8,8 +8,18 @@ fun functionName(parameters): ReturnType{
 fun greet(){
     println("Good Morning!")
 }
-fun sum(a:Int,b:Int):Int{
+fun sum(a:Int,b:Int):Int{ //Without default arguments and return type
     return a+b
+}
+fun sum1(a:Int=10,b:Int=20){ //With default arguments
+    val c=a+b
+    println(c)
+}
+
+
+//Recursive Function
+fun fact(a:Int):Int{
+    return if(a<=1)1 else a*fact(a-1)
 }
 fun main(args:Array<String>){
     println("Calling greet Function")
@@ -17,4 +27,9 @@ fun main(args:Array<String>){
     println("Bye")
     println(sum(2,3))
     println(sum(10,20))
+    //println(sum()) //Error:No value passed for parameter 'a'
+    println()
+    sum1(50,20)
+    sum1()
+    println("Factorial of 5 is ${fact(5)}")
 }

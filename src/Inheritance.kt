137e3562e25d36b3238b1d557205c644cb0ab52e
Nikhil <nenavathnikhil2@gmail.com>
2024-09-05 13:23:35 +0530

    Updation

--- src/Inheritance.kt
+++ src/Inheritance.kt
@@ -1,5 +1,32 @@
+/*
 open class Rectangle(val a:Double,val b:Double){
     fun area():Double{
+        return a*b
+    }
+    open fun display(){
+        println("area of rectangle with dimensions $a and $b is ${area()}")
+    }
+}
+class Square(side:Double):Rectangle(side,side){
+    override fun display(){
+        println("area of square with side $a is ${area()}")
+    }
+}
+fun main()
+{
+    val myRectangle=Rectangle(10.0,5.0)
+    myRectangle.display()
+    var mySquare=Square(1.5)
+    mySquare.display()
+}
+*/
+
+//Interface in Kotlin
+interface Shape{
+    fun area():Double
+}
+open class Rectangle(val a:Double,val b:Double):Shape{
+    override fun area():Double{
         return a*b
     }
     open fun display(){
